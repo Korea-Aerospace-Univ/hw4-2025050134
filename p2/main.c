e0ae3c57ec5b725ec7b777f6890b7740a88a8baa
2025050134 <yoonga06@kau.kr>
2026-04-20 15:59:28 +0900

    Update main.c

--- p2/main.c
+++ p2/main.c
@@ -2,23 +2,23 @@
 
 int main(void) {
     int N;
-    char str[101];
+    char string[101];
     
     int max_lower = 0, count_lower = 0;
     int max_digit = 0, count_digit = 0;
 
     scanf("%d", &N);
-    scanf("%s", str);
+    scanf("%s", string);
 
     for (int i = 0; i < N; i++) {
-        if (str[i] >= 'a' && str[i] <= 'z') {
+        if (string[i] >= 'a' && string[i] <= 'z') {
             count_lower++;
             if (count_lower > max_lower) {
                 max_lower = count_lower;
             }
             count_digit = 0;
         }
-        else if (str[i] >= '0' && str[i] <= '9') {
+        else if (string[i] >= '0' && string[i] <= '9') {
             count_digit++;
             if (count_digit > max_digit) {
                 max_digit = count_digit;

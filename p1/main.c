7db738c11ecafd86cf547a17ddb386f1611df668
2025050134 <yoonga06@kau.kr>
2026-04-20 15:28:57 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -2,11 +2,13 @@
 
 int main(void) {
     int answer, num;
+    int count = 0;
 
     scanf("%d", &answer);
 
     do {
         scanf("%d", &num);
+        count++;
 
         if (num > answer) {
             printf("%d>?\n", num);
@@ -14,9 +16,10 @@ int main(void) {
             printf("%d<?\n", num);
         } else {
             printf("%d==?\n", num);
-            printf("%d\n", num);
         }
     } while (num != answer);
+
+    printf("%d\n", count);
 
     return 0;
 }
